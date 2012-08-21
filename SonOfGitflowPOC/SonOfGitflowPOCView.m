50aabb7bc39f5713b8ca7463a3cbf9873970c12a
Daniel Landgraf <dlandgraf@snl.com>
2012-08-21 10:03:47 -0600

    Added UIView

---
+++ SonOfGitflowPOC/SonOfGitflowPOCView.m
@@ -0,0 +1,31 @@
+//
+//  SonOfGitflowPOCView.m
+//  SonOfGitflowPOC
+//
+//  Created by Daniel  Landgraf on 8/21/12.
+//  Copyright (c) 2012 SNL Financial. All rights reserved.
+//
+
+#import "SonOfGitflowPOCView.h"
+
+@implementation SonOfGitflowPOCView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
