ebb0c2368c5d91b60da1551a00379ce0f433b2ac
Daniel Landgraf <dlandgraf@snl.com>
2012-08-21 12:44:35 -0600

    Added second controller

---
+++ SonOfGitflowPOC/SecondLevel/SonOfGitflowPOCViewController2.m
@@ -0,0 +1,44 @@
+//
+//  SonOfGitflowPOCViewController2.m
+//  SonOfGitflowPOC
+//
+//  Created by Daniel  Landgraf on 8/21/12.
+//  Copyright (c) 2012 SNL Financial. All rights reserved.
+//
+
+#import "SonOfGitflowPOCViewController2.h"
+
+@interface SonOfGitflowPOCViewController2 ()
+
+@end
+
+@implementation SonOfGitflowPOCViewController2
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+	return YES;
+}
+
+@end
