b288f26ff56b6e3bbd6eee12a4d666bc68c6a05a
Daniel Landgraf <dlandgraf@snl.com>
2012-10-11 16:08:35 -0600

    Added another test

--- SonOfGitflowPOCTests/SonOfGitflowPOCTests.m
+++ SonOfGitflowPOCTests/SonOfGitflowPOCTests.m
@@ -103,4 +103,12 @@
 
 }
 
+- (void) testControllerMoreLoad
+{
+    // Blah, blah....
+    NSDictionary *testDictionary;
+    STAssertNil(testDictionary, @"This test dictionary has not been used and therefore should be nil.");
+
+}
+
 @end
